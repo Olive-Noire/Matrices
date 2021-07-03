49e2fd47ca70cc739a702fa7c84ad72f2d55f77b
Olive <Olive.bien.noir@gmail.com>
2021-07-03 16:29:33 +0200

    Prototypes + Signatures d'implémentations + Tests unitaires

--- Code/Headers/Matrice.hpp
+++ Code/Headers/Matrice.hpp
@@ -1,9 +1,46 @@
 #ifndef DEF_MATRICE_HPP
 #define DEF_MATRICE_HPP
 
+#include <ostream>
+#include <vector>
+
 class Matrice {
 
-    
+    public:
+
+    Matrice(const Matrice&) = default;
+    Matrice(Matrice&&) noexcept = default;
+
+    Matrice(std::size_t, std::size_t) noexcept;
+
+    ~Matrice() = default;
+
+    std::size_t Lines() const noexcept;
+    std::size_t Columns() const noexcept;
+
+    Matrice Transpose() const noexcept;
+
+    friend Matrice operator+(const Matrice&, const Matrice&) noexcept;
+
+    friend Matrice operator*(const Matrice&, const Matrice&) noexcept;
+    friend Matrice operator*(const Matrice&, int) noexcept;
+
+    Matrice& operator+=(const Matrice&) noexcept;
+
+    Matrice& operator*=(const Matrice&) noexcept;
+    Matrice& operator*=(int) noexcept;
+
+    friend std::ostream& operator<<(std::ostream&, const Matrice&);
+
+    int& operator()(std::size_t, std::size_t) const;
+    int& operator()(std::size_t, std::size_t);
+
+    Matrice& operator=(const Matrice&) = default;
+    Matrice& operator=(Matrice&&) noexcept = default;
+
+    private:
+
+    std::vector<int> m_content;
 
 };
 
--- Code/Sources/Matrice.cpp
+++ Code/Sources/Matrice.cpp
@@ -1,1 +1,65 @@
 #include "../Headers/Matrice.hpp"
+
+Matrice::Matrice(std::size_t x, std::size_t y) noexcept {
+
+
+
+}
+
+std::size_t Matrice::Lines() const noexcept {}
+std::size_t Matrice::Columns() const noexcept {}
+
+Matrice Matrice::Transpose() const noexcept {
+
+
+
+}
+
+Matrice operator+(const Matrice &l, const Matrice &r) noexcept {
+
+
+
+}
+
+Matrice operator*(const Matrice &l, const Matrice &r) noexcept {
+
+
+
+}
+
+Matrice operator*(const Matrice &m, int n) noexcept {
+
+
+
+}
+
+Matrice& Matrice::operator+=(const Matrice &m) noexcept {
+
+    *this = *this+m;
+    return *this;
+
+}
+
+Matrice& Matrice::operator*=(const Matrice &m) noexcept {
+
+    *this = *this*m;
+    return *this;
+
+}
+
+Matrice& Matrice::operator*=(int n) noexcept {
+
+    *this = *this*n;
+    return *this;
+
+}
+
+std::ostream& operator<<(std::ostream &flux, const Matrice &m) {
+
+    flux << "";
+    return flux;
+
+}
+
+int& Matrice::operator()(std::size_t x, std::size_t y) const {}
+int& Matrice::operator()(std::size_t x, std::size_t y) {}
